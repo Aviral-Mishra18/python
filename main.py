4c3359427b6502cb580b49653fd873d58bf64713
Aviral Mishra <avi.avimishra.com@gmail.com>
2026-05-31 01:26:37 +0530

    main.py

---
+++ main.py
@@ -0,0 +1,66 @@
+"""strings"""
+
+#a = "SHER CODER"
+#print(a[0:4:1]) # 4 likha kyuki woo 1 kam chlta hai  
+
+#print(a[5:10])
+
+#print(a[::])
+
+"""Type Conversion"""
+
+#a= "12"
+
+#a = int(a)
+
+#print(type(a))
+
+
+#a = 0
+
+#print(bool(a))
+
+#IF else:
+
+# a = 6
+
+# if a > 10:
+#     print("I am here")
+
+# else:
+#     print("I am not here")
+
+# money = int(input("please provide me the money :-"))
+
+# if money == 10:
+#     print("I will have a choco bar icecream")
+
+# elif money == 20:
+#     print("I will have a mangodolly")
+
+# elif money == 30:
+#     print("I will have a frosty")    
+
+# else:
+#     print("I will have a cone icecream")
+
+
+# from re import A
+
+
+# num1 = int(input("please tell your first number"))
+# num2 = int(input("please tell your second number")) 
+
+# if num1 > num2:
+#     print(f"{num1} is greater than {num2}")
+
+# elif num2 > num1:
+#     print(f"{num2} is greater than {num1}")
+
+# else:
+#     print("Both the numbers are same") 
+
+
+
+
+ 
